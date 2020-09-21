31355f3fa1495d0b4a80ea6b5f065f59bd92895d
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-21 18:10:34 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -7,7 +7,7 @@ const Review = ({
     summary_short
   }) => {
     return (
-  
+
       <div
         key={headline}
         className="review"
@@ -26,9 +26,9 @@ const Review = ({
       </div>
     );
   };
-  
+
   const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
-  
-  
+
+
 
  export default MovieReviews
